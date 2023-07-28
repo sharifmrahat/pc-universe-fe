d9301d9a164795838e445bcc3d0f1675c51cfc8a
sharifmrahat <sharifmrahat@gmail.com>
2023-07-28 16:23:08 +0600

    font colors and header component is added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ style: "normal", weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={poppins.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
